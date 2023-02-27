f6a607c61e6d2697726fea235f3dfe7e6a6d1887
Thomas <tomiishigo@gmail.com>
2023-02-26 21:32:56 -0800

    update

--- include/Game.hpp
+++ include/Game.hpp
@@ -8,17 +8,38 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 
+#include <iostream>
+#include <vector>
+#include <math.h>
+
 #define WIN_WIDTH 1080
 #define WIN_HEIGHT 720
+
+class TriangleShape : public sf::ConvexShape
+{
+public:
+    explicit TriangleShape(float length)
+    {
+        m_length = length;
+        setPointCount(3);
+        setPoint(0, sf::Vector2f(m_length, m_length));
+        setPoint(1, sf::Vector2f(0, m_length));
+        setPoint(2, sf::Vector2f(m_length / 2, sqrt((3 / 4) * m_length * m_length)));
+    }
+
+private:
+    float m_length;
+};
 
 class Game
 {
 public:
     static void start();
 
+
+
 private:
     static void load();
-    static void resetTimer();
     static void handleInputs();
     static void update();
     static void render();
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,31 +1,48 @@
 #include "Game.hpp"
-#include <iostream>
 
 // ===== Testing =====
 
-sf::CircleShape circle(10);
+sf::CircleShape player(20);
+TriangleShape t1(30);
+TriangleShape t2(30);
+TriangleShape t3(30);
+TriangleShape t4(30);
 
-void testRender(sf::RenderWindow *m_window) {
-    m_window->draw(circle);
+void getDistance() {
+    
 }
 
 void testLoad() {
-    circle.setFillColor(sf::Color::Green);
-    circle.setPosition(20, 20);
+    player.setPosition(200, 200);
+    player.setFillColor(sf::Color::Green);
+
+    t1.setFillColor(sf::Color::Blue);
+    t2.setPosition(150, 150);
+    t3.setPosition(100, 150);
+    t4.setPosition(150, 100);
+}
+
+void testRender(sf::RenderWindow *m_window) {
+    m_window->draw(player);
+    m_window->draw(t1);
+    m_window->draw(t2);
+    m_window->draw(t3);
+    m_window->draw(t4);
 }
 
 void handleKeyMove(sf::Time m_time) {
+    float speed = 150;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-        circle.move(sf::Vector2f(-1 * m_time.asSeconds() * 100, 0));
+        player.move(sf::Vector2f(-1 * m_time.asSeconds() * speed, 0));
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-        circle.move(sf::Vector2f(0, -1 * m_time.asSeconds() * 100));
+        player.move(sf::Vector2f(0, -1 * m_time.asSeconds() * speed));
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-        circle.move(sf::Vector2f(m_time.asSeconds() * 100, 0));
+        player.move(sf::Vector2f(m_time.asSeconds() * speed, 0));
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
-        circle.move(sf::Vector2f(0, m_time.asSeconds() * 100));
+        player.move(sf::Vector2f(0, m_time.asSeconds() * speed));
     }
 }
 
@@ -50,7 +67,7 @@ void Game::start() {
 
 void Game::load() {
     m_window.create(sf::VideoMode(WIN_WIDTH, WIN_HEIGHT), "LL");
-    
+    testLoad();
 }
 
 void Game::handleInputs() {
@@ -61,7 +78,13 @@ void Game::handleInputs() {
                 quit = true;
                 break;
             case sf::Event::KeyPressed:
-
+                switch (event.key.code) {
+                    case sf::Keyboard::Escape:
+                        quit = true;
+                        break;
+                    default:
+                        break;
+                }
                 break;
             default:
                 break;
